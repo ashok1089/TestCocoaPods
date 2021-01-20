b02ac463c72c88d41f7fe03f3087621b4e9cc0ec
Ashok Chikkam <ashokc1089@gmail.com>
2021-01-20 17:30:33 -0500

    initial commit

---
+++ TestCocoaPods/TestCocoaPods/PrintService.swift
@@ -0,0 +1,17 @@
+//
+//  PrintService.swift
+//  TestCocoaPods
+//
+//  Created by Ashok Chikkam on 1/20/21.
+//
+
+import Foundation
+
+public class PrintService {
+    
+    private init() { }
+    
+    public static func printStatement() -> String {
+            return "returning print statement from initial version"
+    }
+}
---
+++ TestCocoaPods/TestCocoaPods/TestCocoaPods.h
@@ -0,0 +1,18 @@
+//
+//  TestCocoaPods.h
+//  TestCocoaPods
+//
+//  Created by Ashok Chikkam on 1/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCocoaPods.
+FOUNDATION_EXPORT double TestCocoaPodsVersionNumber;
+
+//! Project version string for TestCocoaPods.
+FOUNDATION_EXPORT const unsigned char TestCocoaPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoaPods/PublicHeader.h>
+
+
